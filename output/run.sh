e6e5d9d005901ed68c12fd0027bf830eef18ebf7
Tyler Bell <tyler@tylerwbell.com>
2014-10-04 16:50:02 -0400

    script auto open ast.png

--- output/run.sh
+++ output/run.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 javac -classpath .:antlr.jar:string.jar:antlr-dep.jar tigerParser.java tigerLexer.java Main.java
 java -classpath antlr.jar:string.jar:antlr-dep.jar:. Main
-dot -Tpng input.dot -o test.png
-
+dot -Tpng input.dot -o ast.png
+if hash open 2>/dev/null; then
+    open "ast.png"
+fi
