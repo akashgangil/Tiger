c06df2f29060ab703a5ce92c264a529d21b53ac6
Akash Gangil <akashg1611@gmail.com>
2014-10-04 13:37:51 -0400

    add dot command to run.sh

--- output/run.sh
+++ output/run.sh
@@ -1,2 +1,4 @@
 javac -classpath .:antlr.jar:string.jar:antlr-dep.jar tigerParser.java tigerLexer.java Main.java
 java -classpath antlr.jar:string.jar:antlr-dep.jar:. Main
+dot -Tpng input.dot -o test.png
+
