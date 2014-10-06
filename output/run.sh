762291cd3d029af07ddb556ade90bff6644b2ff4
Akash Gangil <akashg1611@gmail.com>
2014-10-06 16:20:50 -0400

    Removing the open in the shell script As it may cause error stating Couldn't get a file descriptor referring to the console. This occurs when the default program is not installed on the machine

--- output/run.sh
+++ output/run.sh
@@ -26,6 +26,3 @@ echo $extension
 javac -d ./classes/ -classpath antlr-runtime.jar:antlr-stringtemplate.jar:antlr.jar:. TigerParser.java TigerLexer.java Tiger.java
 java -classpath antlr-runtime.jar:antlr-stringtemplate.jar:antlr.jar:./classes/ Tiger "$filename.tiger"  
 dot -Tpng "$filename.dot" -o "$filename.png"
-if hash open 2>/dev/null; then
-    open "$filename.png"
-fi
