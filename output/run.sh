8c95bfed395f251ab6bb57d0e6baab7dc640bb5d
Akash Gangil <akashg1611@gmail.com>
2014-10-06 11:45:35 -0400

    fix run.sh

--- output/run.sh
+++ output/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-javac -classpath antlr-runtime.jar:antlr-stringtemplate.jar:antlr.jar:. tigerParser.java tigerLexer.java Tiger.java
+javac -classpath antlr-runtime.jar:antlr-stringtemplate.jar:antlr.jar:. TigerParser.java TigerLexer.java Tiger.java
 java -classpath antlr-runtime.jar:antlr-stringtemplate.jar:antlr.jar:. Tiger 
 dot -Tpng ast.dot -o ast.png
 if hash open 2>/dev/null; then
