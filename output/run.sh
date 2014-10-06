d2ba77fadd744e842091ba9e23bc81889bbd9b71
Akash Gangil <akashg1611@gmail.com>
2014-10-06 11:52:27 -0400

    generate all the classes in the classes folder

--- output/run.sh
+++ output/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-javac -classpath antlr-runtime.jar:antlr-stringtemplate.jar:antlr.jar:. TigerParser.java TigerLexer.java Tiger.java
-java -classpath antlr-runtime.jar:antlr-stringtemplate.jar:antlr.jar:. Tiger 
+javac -d ./classes/ -classpath antlr-runtime.jar:antlr-stringtemplate.jar:antlr.jar:. TigerParser.java TigerLexer.java Tiger.java
+java -classpath antlr-runtime.jar:antlr-stringtemplate.jar:antlr.jar:./classes/ Tiger 
 dot -Tpng ast.dot -o ast.png
 if hash open 2>/dev/null; then
     open "ast.png"
