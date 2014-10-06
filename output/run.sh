a95cf6855b3a4fd3d973c156143eb5e7cee76bf0
Akash Gangil <akashg1611@gmail.com>
2014-10-06 14:21:38 -0400

    add checks and give user an error

--- output/run.sh
+++ output/run.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <INPUT TIGER PROGRAM FILE>" >&2
+  exit 1
+fi
+if ! [ -e "$1" ]; then
+  echo "$1 not found" >&2
+  exit 1
+fi
+if ! [ -f "$1" ]; then
+  echo "$1 not a file" >&2
+  exit 1
+fi
+
+
 fullfile=$1
 filename=$(basename "$fullfile")
 extension="${filename##*.}"
