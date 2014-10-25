c5f0320fb127634fa72619475860c65dfe615fd4
Akash Gangil <akashg1611@gmail.com>
2014-10-25 01:57:02 -0400

    symbol table code init

--- src/Tiger.java
+++ src/Tiger.java
@@ -18,6 +18,9 @@ import java.util.List;
 
 public class Tiger {
 
+    private static int currentLexicalLevel = 0;
+    private static String currentProcName = "Global";
+
     private static class Args{
         String name;
         String type;
@@ -31,6 +34,8 @@ public class Tiger {
     private static  class Symbol{
         String name;
         String type;
+        String dataType;
+        boolean isAggregate;
         String declaringProc;//none for global variables
         int lexicalLevel;
         int func_num_args;//only applicable for functions
@@ -38,23 +43,24 @@ public class Tiger {
         
         Symbol next;
 
-        public Symbol(String name, String type, String declaringProc, 
-                      int lexicalLevel, int func_num_args, List<Args> arg_list){
+        public Symbol(String name, String type, String dataType, String declaringProc, 
+                      int lexicalLevel, int func_num_args, List<Args> arg_list
+                      ,boolean isAggregate, Symbol next){
             this.name = name;
             this.type = type;
+            this.dataType = dataType; //variable dataTypes
             this.declaringProc = declaringProc;
             this.lexicalLevel = lexicalLevel;
             this.func_num_args = func_num_args;
             this.arg_list = arg_list;
-
-            //is null for the first identifier occurence
-            this.next = null;
-
-        } 
+            this.isAggregate = isAggregate;
+            this.next = next; 
+       } 
             
         public String toString(){
-            return name + " " + type + " " + declaringProc + " " 
-                   + lexicalLevel + " " + func_num_args + " " + arg_list.size();
+            return name + " " + type + " " + dataType + " " + declaringProc + " " 
+                   + lexicalLevel + " " + func_num_args + " " + arg_list.size()
+                   + " " + isAggregate;
         }
     }
 
@@ -62,7 +68,73 @@ public class Tiger {
     private static void printSymbolTable(Hashtable<String, Symbol> ht){
         System.out.println(ht); 
     }
-    
+   
+
+    private static void handleNode(CommonTree node){
+        String ASTLabel = node.getText();
+        switch(ASTLabel){
+            case "PROGRAM": break; //do nothing
+            case "TYPES":
+                         System.out.println("TYPES FOUND!");
+                         //Process the list of "TYPES"
+                         if(node.getChildren() == null) break;
+                         System.out.println("Processing");
+                         for(Object children : node.getChildren()){
+                            CommonTree child = (CommonTree)children;
+                            String type = "UserDefinedDataType";
+                            String name = "no name set!";
+                            for(Object sc: child.getChildren()){
+                                CommonTree subChildren = (CommonTree)sc;
+                                if(subChildren.childIndex == 0){
+                                    name = subChildren.getText();
+                                     System.out.println("Child 0 " + name);
+                                }
+                                else if(subChildren.childIndex == 1){
+                                    System.out.println("Child 1 ");
+                                    String dataType = subChildren.getText();
+                                    if(dataType.equals("int") || dataType.equals("fixedpt")){
+                                        ht.put(name, new Symbol(name, type, dataType, currentProcName, 
+                                                                currentLexicalLevel, 0, Collections.EMPTY_LIST, 
+                                                                false, null));
+                                    }
+                                    else if(dataType.equals("array")){
+                                        System.out.println("Hey I came here!!");
+                                        if(subChildren.getChildCount() == 3){
+                                        int dim1 = Integer.parseInt(subChildren.getChild(0).getText());
+                                        int dim2 = Integer.parseInt(subChildren.getChild(1).getText());
+                                        String arrayDataType = subChildren.getChild(2).getText();
+                                        ht.put(name, new Symbol(name, type, dataType, currentProcName, 
+                                                                currentLexicalLevel, 0, Collections.EMPTY_LIST, 
+                                                                true, null));
+
+                                       }
+                                       else if(subChildren.getChildCount() == 2){
+                                        ht.put(name, new Symbol(name, type, dataType, currentProcName, 
+                                                                currentLexicalLevel, 0, Collections.EMPTY_LIST, 
+                                                                true, null));
+
+                                       } 
+                                    }
+                                }
+                            }                     
+                         }
+                         break; 
+
+
+            case "FUNCTIONS": break;
+            case "PARAMS":  break;
+            case "VARIABLES": break;
+            case "STATEMENTS": break;
+            case "BLOCK": break;
+            case "FOR": break;
+            case "INVOKE": break;
+            case "EXPRS": break;
+            case "EXPR": break;
+            case "REFERENCE":  break;  
+            default: break;
+        }
+    }
+
 
     private static void bfs(CommonTree root){
         Queue q = new LinkedList();
@@ -71,6 +143,9 @@ public class Tiger {
         while(q.peek() != null){
             CommonTree node = (CommonTree)q.remove();
             System.out.println("Node: "+ node.getText());
+            
+            handleNode(node);
+
             if (node.getChildren() != null) {
                 for(Object t: node.getChildren()){
                     if(t != null)
@@ -121,9 +196,6 @@ public class Tiger {
             bfs(tree);
             //print sample symbol table
             System.out.println("Sample Hash Table");
-            ht.put("test", new Symbol("test_name", "test_type", "test_proc",
-                                      0, 0, Collections.EMPTY_LIST)
-            ); 
             printSymbolTable(ht);
 
 
