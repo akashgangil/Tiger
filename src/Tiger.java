1675ba8af8f19de38c86e0c793ecf1a9d52adba0
Akash Gangil <akashg1611@gmail.com>
2014-10-25 11:19:04 -0400

    define base classes and structure

--- src/Tiger.java
+++ src/Tiger.java
@@ -18,107 +18,221 @@ import java.util.List;
 
 public class Tiger {
 
-    private static int currentLexicalLevel = 0;
-    private static String currentProcName = "Global";
+    private static int CURRENT_LEXICAL_LEVEL = 0;
+    private static String CURRENT_PROC_NAME = "GLOBAL";
 
-    private static class Args{
-        String name;
-        String type;
+    private enum ASTTokens{
+        VAR, FUNCTION, TYPE
+    }
+    private static class Arg{
+        private String name;
+        private String type;
+        private String value;
 
-        public Args(String name, String type){
+        public Arg(String name, String type, String value){
             this.name = name;
             this.type = type;
+            this.value = value;
+        }
+
+        public String toString(){
+            return "Arg: [Name: " + name + " ], [Type: " + type
+                   + "], [Value: " + value + "]";
+        }
+    }
+    
+    private static class Symbol{
+        private String name;
+
+        public Symbol(String name){
+            this.name = name;
+        }
+
+        public String toString(){
+            return "[SymbolName: " + name;
         }
     }
 
-    private static  class Symbol{
-        String name;
-        String type;
-        String dataType;
-        boolean isAggregate;
-        String declaringProc;//none for global variables
-        int lexicalLevel;
-        int func_num_args;//only applicable for functions
-        List<Args> arg_list;//only applicable for functions
-        
-        Symbol next;
+    private static class SymbolInfo{
+        private String value;
+        private ASTTokens type;
+        private String declaringProc;
+        private int lexicalLevel;
+        private List<SymbolInfo> next;
 
-        public Symbol(String name, String type, String dataType, String declaringProc, 
-                      int lexicalLevel, int func_num_args, List<Args> arg_list
-                      ,boolean isAggregate, Symbol next){
-            this.name = name;
+        public SymbolInfo(ASTTokens type, String declaringProc, int lexicalLevel){
             this.type = type;
-            this.dataType = dataType; //variable dataTypes
             this.declaringProc = declaringProc;
             this.lexicalLevel = lexicalLevel;
-            this.func_num_args = func_num_args;
-            this.arg_list = arg_list;
-            this.isAggregate = isAggregate;
-            this.next = next; 
-       } 
-            
+            this.next = Collections.EMPTY_LIST;
+        }
+
         public String toString(){
-            return name + " " + type + " " + dataType + " " + declaringProc + " " 
-                   + lexicalLevel + " " + func_num_args + " " + arg_list.size()
-                   + " " + isAggregate;
+            return "[Type: " + 
+                type + "], [DP: " + 
+                declaringProc + "], LL: " + 
+                lexicalLevel + "], Chain: " +
+                next;
+        }
+
+        //We will chain if we can same names
+        public void chainSymbol(SymbolInfo chainElement){
+            next.add(chainElement);
+        }
+
+        public void setValue(String value){
+            this.value = value;
         }
     }
 
-    private static Hashtable<String, Symbol> ht = new Hashtable<String, Symbol>();
-    private static void printSymbolTable(Hashtable<String, Symbol> ht){
+    //variables VAR
+    private static class VariableInfo extends SymbolInfo{
+        private String dataType;
+        private int value; //since we have only int and fixedpt as possible datatypes
+
+        public VariableInfo(String declaringProc, 
+                int lexicalLevel, String dataType, int value){
+            super(ASTTokens.VAR, declaringProc, lexicalLevel);
+            setValue(value);
+            setDataType(dataType);
+        }
+
+        private void setValue(int value){
+            this.value = value;
+        }
+
+        private void setDataType(String dataType){
+            this.dataType = dataType;
+        }
+
+        public String toString(){
+            return super.toString() + " [DataType: " +
+                dataType + "], [Value: " +
+                value;
+        }
+    }
+
+
+    //functions FUNCTION
+    private static class FunctionInfo extends SymbolInfo{
+        private String returnType;
+        private int numArgs;
+        private List<Arg> argList;
+
+        public FunctionInfo(String returnType, int numArgs,
+                List<Arg> argList){
+            super(ASTTokens.FUNCTION, CURRENT_PROC_NAME, CURRENT_LEXICAL_LEVEL);
+            setReturnType(returnType);
+            setNumArgs(numArgs);
+            setArgList(argList);
+        }
+
+        private void setReturnType(String returnType){
+            this.returnType = returnType;
+        }
+
+        private void setNumArgs(int numArgs){
+            this.numArgs = numArgs;
+        }
+
+        private void setArgList(List<Arg> argList){
+            this.argList = argList;
+        }
+
+        public String toString(){
+            return super.toString() + " [RT: " +
+                returnType + " ], [NumArgs: " + 
+                numArgs + " ], [ArgList: " +
+                argList;
+        }
+    }
+
+    //user defined data types TYPES
+    private static class DataTypeInfo extends SymbolInfo{
+        private boolean isAggregate;
+        private String baseDataType;
+        private int dim1;
+        private int dim2;
+
+        public DataTypeInfo(boolean isAggregate, String baseDataType, int dim1, int dim2){
+            super(ASTTokens.TYPE, CURRENT_PROC_NAME, CURRENT_LEXICAL_LEVEL);
+            setIsAggregate(isAggregate);
+            setBaseDataType(baseDataType);
+            setDimension1(dim1);
+            setDimension2(dim2);
+        }
+
+        private void setIsAggregate(boolean isAggregate){
+            this.isAggregate = isAggregate;
+        }
+
+        private void setBaseDataType(String baseDataType){
+            this.baseDataType = baseDataType;
+        }
+
+        private void setDimension1(int dim1){
+            this.dim1 = dim1;
+        }
+
+        private void setDimension2(int dim2){
+            this.dim2 = dim2;
+        }
+
+        public String toString(){
+            return super.toString() + " [isAggregate: " + isAggregate 
+                    + " ], [baseDataType: " + baseDataType + " ], [Dim1: " +
+                    dim1 + " ], [Dim2: " + dim2 + "] ";
+        }
+    }
+
+    private static Hashtable<Symbol, SymbolInfo> ht = new Hashtable<Symbol, SymbolInfo>();
+    private static void printSymbolTable(Hashtable<Symbol, SymbolInfo> ht){
         System.out.println(ht); 
     }
-   
+
 
     private static void handleNode(CommonTree node){
         String ASTLabel = node.getText();
         switch(ASTLabel){
             case "PROGRAM": break; //do nothing
             case "TYPES":
-                         System.out.println("TYPES FOUND!");
-                         //Process the list of "TYPES"
-                         if(node.getChildren() == null) break;
-                         System.out.println("Processing");
-                         for(Object children : node.getChildren()){
-                            CommonTree child = (CommonTree)children;
-                            String type = "UserDefinedDataType";
-                            String name = "no name set!";
-                            for(Object sc: child.getChildren()){
-                                CommonTree subChildren = (CommonTree)sc;
-                                if(subChildren.childIndex == 0){
-                                    name = subChildren.getText();
-                                     System.out.println("Child 0 " + name);
-                                }
-                                else if(subChildren.childIndex == 1){
-                                    System.out.println("Child 1 ");
-                                    String dataType = subChildren.getText();
-                                    if(dataType.equals("int") || dataType.equals("fixedpt")){
-                                        ht.put(name, new Symbol(name, type, dataType, currentProcName, 
-                                                                currentLexicalLevel, 0, Collections.EMPTY_LIST, 
-                                                                false, null));
+                            System.out.println("TYPES FOUND!");
+                            //Process the list of "TYPES"
+                            if(node.getChildren() == null) break;
+                            System.out.println("Processing");
+                            for(Object children : node.getChildren()){
+                                CommonTree child = (CommonTree)children;
+                                String type = "UserDefinedDataType";
+                                String name = "NoNameDefined";
+                                for(Object sc: child.getChildren()){
+                                    CommonTree subChildren = (CommonTree)sc;
+                                    if(subChildren.childIndex == 0){
+                                        name = subChildren.getText();
+                                        System.out.println("Child 0 " + name);
                                     }
-                                    else if(dataType.equals("array")){
-                                        System.out.println("Hey I came here!!");
-                                        if(subChildren.getChildCount() == 3){
-                                        int dim1 = Integer.parseInt(subChildren.getChild(0).getText());
-                                        int dim2 = Integer.parseInt(subChildren.getChild(1).getText());
-                                        String arrayDataType = subChildren.getChild(2).getText();
-                                        ht.put(name, new Symbol(name, type, dataType, currentProcName, 
-                                                                currentLexicalLevel, 0, Collections.EMPTY_LIST, 
-                                                                true, null));
-
-                                       }
-                                       else if(subChildren.getChildCount() == 2){
-                                        ht.put(name, new Symbol(name, type, dataType, currentProcName, 
-                                                                currentLexicalLevel, 0, Collections.EMPTY_LIST, 
-                                                                true, null));
-
-                                       } 
+                                    else if(subChildren.childIndex == 1){
+                                        System.out.println("Child 1 ");
+                                        String dataType = subChildren.getText();
+                                        if(dataType.equals("int") || dataType.equals("fixedpt")){
+                                            ht.put(new Symbol(name), new DataTypeInfo(false, dataType, -1, -1)); 
+                                        }
+                                        else if(dataType.equals("array")){
+                                            if(subChildren.getChildCount() == 3){
+                                                int dim1 = Integer.parseInt(subChildren.getChild(0).getText());
+                                                int dim2 = Integer.parseInt(subChildren.getChild(1).getText());
+                                                String arrayDataType = subChildren.getChild(2).getText();
+                                                ht.put(new Symbol(name), new DataTypeInfo(true, arrayDataType, dim1, dim2)); 
+                                            }
+                                            else if(subChildren.getChildCount() == 2){
+                                                int dim1 = Integer.parseInt(subChildren.getChild(0).getText());
+                                                String arrayDataType = subChildren.getChild(1).getText();
+                                                ht.put(new Symbol(name), new DataTypeInfo(true, arrayDataType, dim1, -1)); 
+                                            } 
+                                        }
                                     }
-                                }
-                            }                     
-                         }
-                         break; 
+                                }                     
+                            }
+                            break; 
 
 
             case "FUNCTIONS": break;
@@ -143,7 +257,7 @@ public class Tiger {
         while(q.peek() != null){
             CommonTree node = (CommonTree)q.remove();
             System.out.println("Node: "+ node.getText());
-            
+
             handleNode(node);
 
             if (node.getChildren() != null) {
@@ -176,11 +290,11 @@ public class Tiger {
             String src = readFile(args[0], StandardCharsets.UTF_8);
             TigerLexer lexer = new TigerLexer(new ANTLRStringStream(src));
             TigerParser parser = new TigerParser(new CommonTokenStream(lexer));
-            
+
             /*To print lexer symbols of the program*/
             TigerLexer lexer1 = new TigerLexer(new ANTLRStringStream(src));
             CommonTokenStream cts = new CommonTokenStream(lexer1);
-            
+
             int i = 1;
             Token to = cts.LT(i);
             while(to.getType() != -1){
@@ -188,8 +302,8 @@ public class Tiger {
                 if(to.getType() == -1) break;
                 to = cts.LT(++i);
             }
-            
-           System.out.println();
+
+            System.out.println();
 
             CommonTree tree = (CommonTree)(parser.tiger_program().getTree());
             //run bfs
@@ -201,9 +315,9 @@ public class Tiger {
 
             DOTTreeGenerator gen = new DOTTreeGenerator();
             StringTemplate st = gen.toDOT(tree);
-            
+
             writeDotFile(args[1], st.toString(), StandardCharsets.UTF_8);
-    
+
             if(parser.getErrors().isEmpty() && lexer.getErrors().isEmpty()){
                 System.out.println("***Successful Parse***");
             }
