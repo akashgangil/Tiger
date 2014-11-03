b7ef2d534a0c1a6ffa478c0eff208e15f742b59f
brandonchastain <brandonchastain@gmail.com>
2014-11-03 14:29:21 -0500

    added more nodes to IR gen

--- src/IRGenerator.java
+++ src/IRGenerator.java
@@ -7,6 +7,7 @@ import org.antlr.runtime.tree.*;
 public class IRGenerator {
 
     private int numOfTemps;
+    private int numOfLabels;
 
     public String generate(CommonTree node) {
         List children = node.getChildren();
@@ -37,7 +38,12 @@ public class IRGenerator {
                 return invoke(children);
             case "CONSTANT":
                 return constant(children);
-
+            case "while":
+                return generateWhile(children);
+            case "for":
+                return generateFor(children);
+            case "if":
+                return generateIf(children);
             default:
                 return null;
         }
@@ -72,7 +78,6 @@ public class IRGenerator {
     }
 
     private String array(List children) {
-        //figure out how to get multiplier of the offset
         String r = newTemp();
         String a = generate((CommonTree)children.get(0)); //array
         String o = generate((CommonTree)children.get(1)); //offset
@@ -82,13 +87,13 @@ public class IRGenerator {
     }
 
     private String array2d(List children) {
-        //fix this
+        //TODO: get size of array from symbol table and multiply offset
         String r = newTemp();
         String a = generate((CommonTree)children.get(0));
         String o1 = generate((CommonTree)children.get(1));
         String o2 = generate((CommonTree)children.get(2));
 
-        emit("array_load", a, r);
+        //emit("array_load", a, r);
         return r;
     }
 
@@ -98,20 +103,104 @@ public class IRGenerator {
 
     private String invoke(List children) {
         String a = generate((CommonTree)children.get(0));
-        int length = children.size();
-        ArrayList<String> params = new ArrayList<String>();
-        for (int i = 1; i < length; i++) {
-            String ti = generate((CommonTree)children.get(i));
-            params.add(ti);
-        }
-        String t1 = newTemp();
+        ArrayList<String> params = null;
 
-        emit("call", a, null, t1, params);
+        if (children.size() > 1) {
+            params = new ArrayList<String>();
+            CommonTree exprs = (CommonTree)children.get(1);
+            List exprChildren = exprs.getChildren();
+            int length = exprChildren.size();
+            for (int i = 0; i < length; i++) {
+                String ti = generate((CommonTree)exprChildren.get(i));
+                params.add(ti);
+            }
+        }
+
+        String t1 = newTemp();
+        emit("call", a, t1, null, params);
         return t1;
+    }
+
+    private String generateIf(List children) {
+        String e = newLabel();
+        CommonTree opNode = (CommonTree)children.get(0);
+        String t = generate(opNode);
+        String branchOp = getBranchOp(opNode);
+        String left = generate((CommonTree)opNode.getChildren().get(0));
+        String right = generate((CommonTree)opNode.getChildren().get(1));
+        emit(branchOp, left, right, e);
+
+        generate((CommonTree)children.get(1));
+        emit(e + ":", null, null, null);
+
+        if (children.size() > 2) {
+            generate((CommonTree)children.get(2));
+        }
+
+        return null;
+    }
+
+    private String generateWhile(List children) {
+        String e = newLabel();
+        String t = newLabel();
+        emit(t + ":", null, null, null);
+        CommonTree opNode = (CommonTree)children.get(0);
+        String t1 = generate(opNode);
+        String branchOp = getBranchOp(opNode);
+
+        CommonTree left = (CommonTree)opNode.getChildren().get(0);
+        CommonTree right = (CommonTree)opNode.getChildren().get(1);
+        String t2 = generate(left);
+        String t3 = generate(right);
+        emit(branchOp, t2, t3, e);
+
+        CommonTree stmts = (CommonTree)children.get(1);
+        generate(stmts);
+        emit("goto", t, null, null);
+        emit(e + ":", null, null, null);
+        return null;
+    }
+
+    private String generateFor(List children) {
+        String firstBound = generate((CommonTree)children.get(0));
+        String secondBound = generate((CommonTree)children.get(1));
+        return null;
+    }
+
+    private String getBranchOp(CommonTree opNode) {
+        String op = opNode.getText();
+        String branchOp = null;
+        switch (op) {
+            case "=":
+                branchOp = "brneq";
+                break;
+            case "<>":
+                branchOp = "breq";
+                break;
+            case ">=":
+                branchOp = "brlt";
+                break;
+            case "<=":
+                branchOp = "brgt";
+                break;
+            case "<":
+                branchOp = "brgeq";
+                break;
+            case ">":
+                branchOp = "brleq";
+                break;
+            default:
+                break;
+        }
+        return branchOp;
     }
 
     private String newTemp() {
         return "t" + numOfTemps++;
+    }
+
+    private String newLabel() {
+        return "L" + numOfLabels++;
     }
 
     private void emit(String op, String x, String y) {
