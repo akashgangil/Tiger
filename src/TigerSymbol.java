4bdf4bf1bbf546ef44fc8c60fecede21ee978aa3
Akash Gangil <akashg1611@gmail.com>
2014-11-03 12:52:36 -0500

    add reserved keywords

--- src/TigerSymbol.java
+++ src/TigerSymbol.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.Arrays;
+
 public class TigerSymbol extends TigerAstNode {
     protected String name;
     
@@ -7,5 +10,11 @@ public class TigerSymbol extends TigerAstNode {
     public String getName() {
         return name;
     }
+
+    public static List<String> reservedSymbolNames = Arrays.asList(
+        "print", "printi", "flush", "getchar", "ord", "chr", "size", "substring", "concat", "not", "exit",
+        "function", "begin", "end", "void", "main", "type", "array", "of", "int", "fixedpt", "var", "if",
+        "then", "endif", "else", "while", "do", "enddo", "for", "id", "to", "do", "break", "return"
+    );
 }
 
