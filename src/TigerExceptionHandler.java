39171726bd073e650bb7cc51124d69aca619b343
Akash Gangil <akashg1611@gmail.com>
2014-11-05 14:44:28 -0500

    global static list to hold all semantic exceptions

--- src/TigerExceptionHandler.java
+++ src/TigerExceptionHandler.java
@@ -1,8 +1,12 @@
+import java.util.List;
+import java.util.Collections;
+
 public class TigerExceptionHandler{
     String errorMsg;
     String faultyToken;
     int lineNumber;
-    
+
+    public static List<TigerExceptionHandler> exceptionList = Collections.emptyList();    
     
     public void TigerExceptionHandler(String errorMsg, String faultyToken, int lineNumber){
         this.errorMsg = errorMsg;
