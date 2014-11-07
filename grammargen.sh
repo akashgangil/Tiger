cd8165868ec32b16a886fe4f7d9c7f5549f86a8b
Tyler Bell <tyler@tylerwbell.com>
2014-11-07 18:21:03 -0500

    remove grammar gen script

--- grammargen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-printf "Generating code ... \n"
-java -classpath bin/antlrworks.jar org.antlr.Tool -fo src grammar/Tiger.g &>/dev/null || {
-    printf "Code generation failed. Check antlrworks for reason.\n"
-    exit 1;
-}
-printf "Done\n"
