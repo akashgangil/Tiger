fb8f6acf9c96acb6e970100c92e23aa8c7868723
Akash Gangil <akashg1611@gmail.com>
2014-11-07 18:13:44 -0500

    update master make.sh with antlr grammar compilation

--- make.sh
+++ make.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+printf "Generating code ... \n"
+java -classpath bin/antlrworks.jar org.antlr.Tool -fo src grammar/Tiger.g &>/dev/null || {
+    printf "Code generation failed. Check antlrworks for reason.\n"
+    exit 1;
+}
+printf "Done\n"
+
 mkdir -p build
 
 printf "Compiling ... \n"
