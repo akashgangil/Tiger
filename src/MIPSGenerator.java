5cb64c7cfa596b092e634b63d715ebba122ff79e
Akash Gangil <akashg1611@gmail.com>
2014-11-23 16:51:04 -0500

    start work on MIPS

---
+++ src/MIPSGenerator.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class MIPSGenerator{
+    private List<Quad> mipsCode;
+
+    private final String dataSegment = ".data\n";
+    private final String textSegment = ".text\n";
+    private final String exitProgram = "li $v0, 10\nsyscall\n";
+    
+
+    public MIPSGenerator(){
+        this.mipsCode = new ArrayList<Quad>();
+    }
+
+    public String getMIPSCode(List<Quad> ir){
+       String mipsCode = "";
+       mipsCode += dataSegment;
+
+       mipsCode += textSegment;
+
+       mipsCode += exitProgram;
+       return mipsCode; 
+    }
+}
